68594791d74d40fa44b59db5847217537f38adc0
Karen Etheridge <ether@cpan.org>
2015-07-13 20:24:49 -0700

    increment $VERSION after 0.003 release

--- lib/Dist/Zilla/Plugin/StaticInstall.pm
+++ lib/Dist/Zilla/Plugin/StaticInstall.pm
@@ -5,7 +5,7 @@ package Dist::Zilla::Plugin::StaticInstall;
 # KEYWORDS: distribution metadata toolchain static dynamic installation
 # vim: set ts=8 sts=4 sw=4 tw=115 et :
 
-our $VERSION = '0.003';
+our $VERSION = '0.004';
 
 use Moose;
 with 'Dist::Zilla::Role::MetaProvider',
