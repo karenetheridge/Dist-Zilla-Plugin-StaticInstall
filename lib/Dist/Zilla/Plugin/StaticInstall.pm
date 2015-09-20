e19401dec01f4c4e765971b9e4ef0f03927b83e4
Karen Etheridge <ether@cpan.org>
2015-09-20 11:30:51 -0700

    this section is now generated by my weaver bundle

--- lib/Dist/Zilla/Plugin/StaticInstall.pm
+++ lib/Dist/Zilla/Plugin/StaticInstall.pm
@@ -291,14 +291,6 @@ can see what the value would have been.
 
 =for Pod::Coverage BUILD metadata setup_installer
 
-=head1 SUPPORT
-
-=for stopwords irc
-
-Bugs may be submitted through L<the RT bug tracker|https://rt.cpan.org/Public/Dist/Display.html?Name=Dist-Zilla-Plugin-StaticInstall>
-(or L<bug-Dist-Zilla-Plugin-StaticInstall@rt.cpan.org|mailto:bug-Dist-Zilla-Plugin-StaticInstall@rt.cpan.org>).
-I am also usually active on irc, as 'ether' at C<irc.perl.org>.
-
 =head1 SEE ALSO
 
 =for :list
