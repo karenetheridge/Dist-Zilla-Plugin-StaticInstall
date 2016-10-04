dd2be5b10665ef2a1eb86edec465dcff63a4ec7a
Karen Etheridge <ether@cpan.org>
2016-10-03 18:54:35 -0700

    ensure vim modeline is in the first 5 line

--- lib/Dist/Zilla/Plugin/StaticInstall.pm
+++ lib/Dist/Zilla/Plugin/StaticInstall.pm
@@ -1,9 +1,9 @@
 use strict;
 use warnings;
 package Dist::Zilla::Plugin::StaticInstall;
+# vim: set ts=8 sts=4 sw=4 tw=115 et :
 # ABSTRACT: (EXPERIMENTAL, DANGEROUS) Identify a distribution as eligible for static installation
 # KEYWORDS: distribution metadata toolchain static dynamic installation
-# vim: set ts=8 sts=4 sw=4 tw=115 et :
 
 our $VERSION = '0.011';
 
